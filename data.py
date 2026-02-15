b59b0693de46858c7accc030b2083f32ef497590
SansForSSBU <taylorjoseph3333@gmail.com>
2026-02-15 22:27:40 +0000

    Make all ruff checks pass

--- data.py
+++ data.py
@@ -1,33 +1,33 @@
 game_vers = {
-    b'\x83t\x1e\x0c\x07\xc4\x19\xaf\x14j\xc9Y\xc1\xe6\x81\\': "EU",
-    b'\x0e\xc3G\xb6\xa9nP\xa3\xf6\xbcw\xbfgZ\xb1\x93': "PO",
-    b'\xe8\xd8\xfa5\xff\x9f\xecw\x1b\xfd\xfa\x81\xe1\x0c\xf9\x04': "RU",
-    b'\xa4KgS\x7f+\xec\x16#\xa7\x9bx\xc7\x12\xae\x1b': "US04",
-    b'\xcf2\xa4\x94\x80-\xdb\x0c\xd3S\xac\xa4\xf6D9\x98': "US05"
+    b"\x83t\x1e\x0c\x07\xc4\x19\xaf\x14j\xc9Y\xc1\xe6\x81\\": "EU",
+    b"\x0e\xc3G\xb6\xa9nP\xa3\xf6\xbcw\xbfgZ\xb1\x93": "PO",
+    b"\xe8\xd8\xfa5\xff\x9f\xecw\x1b\xfd\xfa\x81\xe1\x0c\xf9\x04": "RU",
+    b"\xa4KgS\x7f+\xec\x16#\xa7\x9bx\xc7\x12\xae\x1b": "US04",
+    b"\xcf2\xa4\x94\x80-\xdb\x0c\xd3S\xac\xa4\xf6D9\x98": "US05",
 }
 
 cave_offsets = {
-    "EU": 0x1dcd1c,
-    "PO": 0x1924a0,
-    "RU": 0x1924a0,
+    "EU": 0x1DCD1C,
+    "PO": 0x1924A0,
+    "RU": 0x1924A0,
     "US04": 0x191970,
     "US05": 0x191970,
 }
 
 loading_ptrs_hex = {
-    "EU": 0x5c2b9c,
-    "PO": 0x5c3bdc,
-    "RU": 0x5c3bdc,
-    "US04": 0x5c2b9c,
-    "US05": 0x5c2b9c
+    "EU": 0x5C2B9C,
+    "PO": 0x5C3BDC,
+    "RU": 0x5C3BDC,
+    "US04": 0x5C2B9C,
+    "US05": 0x5C2B9C,
 }
 
 dump_addrs = {
-    "US05": 0x005c5f00,
-    "US04": 0x005c5f00,
-    "EU": 0x005ddf00,
-    "RU": 0x005c6f00,
-    "PO": 0x005c6f00,
+    "US05": 0x005C5F00,
+    "US04": 0x005C5F00,
+    "EU": 0x005DDF00,
+    "RU": 0x005C6F00,
+    "PO": 0x005C6F00,
 }
 
 ngplus_offsets = {
--- test/generate_regression_hashes.py
+++ test/generate_regression_hashes.py
@@ -8,20 +8,32 @@ from utils import get_hash
 
 def run_patcher(game_ver, instaload=False, speedfix=False, newgameplus=False):
     overlay = "overlay.exe" if game_ver in ["EU", "US04", "US05"] else "OVERLAY.exe"
-    args = ["python3", "patcher.py", f"test/game_exes/{game_ver}/{overlay}", "test/tmp/overlay.exe"]
-    if instaload: args.append("--instaload")
-    if speedfix: args.append("--speedfix")
-    if newgameplus: args.append("--newgameplus")
+    args = [
+        "python3",
+        "patcher.py",
+        f"test/game_exes/{game_ver}/{overlay}",
+        "test/tmp/overlay.exe",
+    ]
+    if instaload:
+        args.append("--instaload")
+    if speedfix:
+        args.append("--speedfix")
+    if newgameplus:
+        args.append("--newgameplus")
     subprocess.run(args)
+
 
 def generate_regression_hashes(game_ver):
     permutations = list(product([False, True], repeat=3))
     hashes = {}
     for perm in permutations:
         run_patcher(game_ver, instaload=perm[0], speedfix=perm[1], newgameplus=perm[2])
-        hashes[str(perm)] = base64.b64encode(get_hash("test/tmp/overlay.exe")).decode("utf-8")
+        hashes[str(perm)] = base64.b64encode(get_hash("test/tmp/overlay.exe")).decode(
+            "utf-8"
+        )
 
     return hashes
+
 
 game_vers = ["EU", "PO", "RU", "US04", "US05"]
 if __name__ == "__main__":
