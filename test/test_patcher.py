729b364df4df767fdfe37f5269f6c11b07cf1d56
SansForSSBU <taylorjoseph3333@gmail.com>
2026-02-15 15:42:53 +0000

    Make testing of regression hashes test all game versions

--- test/test_patcher.py
+++ test/test_patcher.py
@@ -16,9 +16,10 @@ def run_patcher(ver, instaload=False, speedfix=False, newgameplus=False):
     subprocess.run(args)
 
 def test_regression_hashes():
-    game_ver = "EU"
-    with open(f"test/hashes/{game_ver}.json") as f:
-        verification_hashes = json.load(f)
-    hashes = generate_regression_hashes.generate_regression_hashes(game_ver)
-    for k in verification_hashes:
-        assert hashes[k] == verification_hashes[k]
+    game_vers = ["EU", "PO", "RU", "US04", "US05"]
+    for game_ver in game_vers:
+        with open(f"test/hashes/{game_ver}.json") as f:
+            verification_hashes = json.load(f)
+        hashes = generate_regression_hashes.generate_regression_hashes(game_ver)
+        for k in verification_hashes:
+            assert hashes[k] == verification_hashes[k]
